51c6e5d4593fbf7a23137c531a05b176d29b961c
Oliver S <oliverstevenscoughlan@protonmail.com>
2025-11-02 12:02:09 +0000

    cleanup

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,9 +1,14 @@
-pub use crate::model::{BotCommand, Context, EnvStore, EventListener, GlobalState, get_env};
+pub use crate::model::{
+    CommandRegistry, Context, EnvStore, EventListenerRegistry, GlobalState, get_env,
+};
 pub use crate::{
-    register_commands, register_env, register_event_listener, register_startup_listener,
+    register_commands, register_env, register_event_listener, register_global_data,
+    register_startup_listener,
 };
 pub use anyhow::{Error, Result, bail};
+pub use derive_new::new;
+pub use poise::serenity_prelude::prelude::TypeMap;
 pub use poise::{Command, FrameworkContext, command, serenity_prelude::*};
-pub use std::env;
-pub use tokio::{join, process::Command as ProcessCommand};
+pub use std::{collections::VecDeque, env, sync::Arc};
+pub use tokio::{join, process::Command as ProcessCommand, sync::Mutex, time::Duration};
 pub use tracing::{debug, error, info, instrument, trace, warn};
