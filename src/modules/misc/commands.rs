076626f5d6081228b3924de24adeb5533001d649
Oliver S <oliverstevenscoughlan@protonmail.com>
2025-11-06 21:05:15 +0000

    add source command linking to this repo

--- src/modules/misc/commands.rs
+++ src/modules/misc/commands.rs
@@ -1,6 +1,16 @@
+use poise::CreateReply;
+
 use crate::prelude::*;
 
+register_commands!(source);
+
 #[command(slash_command, prefix_command)]
-pub async fn source(_ctx: Context<'_>) -> Result<()> {
+pub async fn source(ctx: Context<'_>) -> Result<()> {
+    ctx.send(
+        CreateReply::new()
+            .content("<https://github.com/WhaleFromMars/peoplebot-definitive-edition>")
+            .ephemeral(true),
+    )
+    .await?;
     Ok(())
 }
