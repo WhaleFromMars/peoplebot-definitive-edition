f2222f4c3f28c5baacf201e5f151d6247c5f47e4
Oliver S <oliverstevenscoughlan@protonmail.com>
2025-11-09 15:53:35 +0000

    helper for embed module - working out the guilds max attachment size

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,13 +1,19 @@
-// use crate::prelude::*;
+use crate::prelude::*;
 
-// fn attachment_limit(ctx: &Context, guild_id: Option<GuildId>) -> u64 {
-//     let tier = guild_id
-//         .and_then(|id| ctx.cache().guild(id).map(|guild| guild.premium_tier))
-//         .unwrap_or(PremiumTier::Tier0);
+fn attachment_byte_limit(ctx: &Context, guild_id: Option<GuildId>) -> u64 {
+    let tier = guild_id
+        .and_then(|id| {
+            ctx.serenity_context()
+                .cache
+                .guild(id)
+                .map(|guild| guild.premium_tier)
+        })
+        .unwrap_or(PremiumTier::Tier0);
 
-//     match tier {
-//         PremiumTier::Tier2 => 50 * 1_000_000,
-//         PremiumTier::Tier3 => 100 * 1_000_000,
-//         _ => 10 * 1_000_000,
-//     }
-// }
+    match tier {
+        PremiumTier::Tier0 | PremiumTier::Tier1 => 10 * 1_000_000,
+        PremiumTier::Tier2 => 50 * 1_000_000,
+        PremiumTier::Tier3 => 100 * 1_000_000,
+        _ => 10 * 1_000_000,
+    }
+}
